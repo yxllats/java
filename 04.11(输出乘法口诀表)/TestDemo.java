5e706826ed9352cf7b4ab69644320e8d4c14f3e4
yxllats <2357542347@qq.com>
2020-05-04 21:03:45 +0800

    提交代码

---
+++ 04.11(输出乘法口诀表)/TestDemo.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class TestDemo {	
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		int n=scan.nextInt();
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print(i+"*"+j+"="+(i*j)+" ");
+			}
+				System.out.println("");
+		}
+	}
+}
