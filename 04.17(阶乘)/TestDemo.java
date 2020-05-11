5df8c8264421e419b3c23c2d5da672960c2c642b
yxllats <2357542347@qq.com>
2020-05-11 15:24:35 +0800

    提交代码

---
+++ 04.17(阶乘)/TestDemo.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class TestDemo {
+	public static int fact_sum(int n) {
+		int ret=1;
+		for(int i=1;i<=n;i++) {
+			ret*=i;
+		}
+		return ret;
+	}
+	
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		int n=scan.nextInt();
+		int num=fact_sum(n);
+		System.out.println(num);
+	}
+}
