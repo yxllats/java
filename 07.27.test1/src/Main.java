2222274d4de71d927ceb539da18e61e747806a2d
yxllats <2357542347@qq.com>
2020-07-27 19:20:20 +0800

    提交代码

---
+++ 07.27.test1/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3};
+        System.out.println(arr);
+        Integer a = new Integer(3);
+        Integer b = new Integer(3);
+        Integer c = 129;
+        Integer d = 129;
+        System.out.println(c == d);
+        System.out.println(a == b);
+    }
+}
