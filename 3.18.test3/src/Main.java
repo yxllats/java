cd79329014a1bbe421315722a975bce0f843e902
yxllats <2357542347@qq.com>
2021-03-21 21:55:41 +0800

    提交代码

---
+++ 3.18.test3/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        if(n==1||n==2){
+            System.out.println(n);
+            return;
+        }
+        int[] arr = new int[n+1];
+        arr[1] = 1;
+        arr[2] = 2;
+        for (int i = 3; i < n+1; i++) {
+            arr[i] = arr[i-1] + arr[i-2];
+        }
+        System.out.println(arr[n]);
+    }
+}
